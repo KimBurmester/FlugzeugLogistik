8b6024f59beb2d64d0bc2168cfa743a1bac7d47d
Kim Burmester <Kim.Burmester@hotmail.de>
2026-04-19 22:26:39 +0200

    Created a menu for the user and implemented the functionality to open and close it, as well as making some minor responsive adjustments.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const trigger = document.getElementById('header-menu');
 const dropdown = document.getElementById('menu-dropdown');
 
+const user_trigger = document.getElementById('user-menu');
+const user_dropdown = document.getElementById('user-dropdown');
+
+
 trigger.addEventListener('click', () => {
     event.stopPropagation();
     dropdown.classList.toggle('open');
@@ -8,4 +12,14 @@ trigger.addEventListener('click', () => {
 
 document.addEventListener('click', () => {
     dropdown.classList.remove('open');
+    user_dropdown.classList.remove('open'); 
+});
+
+user_trigger.addEventListener('click', () => {
+    event.stopPropagation();
+    user_dropdown.classList.toggle('open');
+});
+
+user_dropdown.addEventListener('click', () => {
+    user_dropdown.classList.remove('open');
 });
